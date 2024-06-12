692ee79dfd54ceac382aa6eb56804c78c183c7dc
Admiral-Simo <admiralsimo16@gmail.com>
2024-06-12 12:01:38 +0100

    feat: remove the portion detection and add partially word detection instead of full sentences

--- internal/adapters/core/ocr/ocr.go
+++ internal/adapters/core/ocr/ocr.go
@@ -3,10 +3,6 @@ package ocr
 import (
 	"bytes"
 	"fmt"
-	"image"
-	"image/draw"
-	"image/jpeg"
-	"os"
 	"os/exec"
 	"passport_card_analyser/internal/adapters/core/types"
 	"passport_card_analyser/internal/adapters/core/utilities"
@@ -26,7 +22,7 @@ func NewParser() *Parser {
 func (p *Parser) GetContent() error {
 	var result bytes.Buffer
 
-	cmd := exec.Command("tesseract", p.image, "-", "stdout")
+	cmd := exec.Command("tesseract", p.image, "-", "stdout", "--psm", "11")
 	cmd.Stdout = &result
 	err := cmd.Run()
 	if err != nil {
@@ -34,58 +30,6 @@ func (p *Parser) GetContent() error {
 	}
 
 	// execute this command and get the ouput into the result variable
-	p.text = result.String()
-	return nil
-}
-
-func (p *Parser) GetContentOfPortion(x, y, width, height int) error {
-	// Open the image file
-	file, err := os.Open(p.image)
-	if err != nil {
-		return fmt.Errorf("error opening image: %w", err)
-	}
-	defer file.Close()
-
-	// Decode the image
-	img, _, err := image.Decode(file)
-	if err != nil {
-		return fmt.Errorf("error decoding image: %w", err)
-	}
-
-	// Define the cropping rectangle
-	rect := image.Rect(x, y, x+width, y+height)
-
-	// Create a new image for the cropped portion
-	croppedImg := image.NewRGBA(rect)
-
-	// Draw the cropped portion onto the new image
-	draw.Draw(croppedImg, rect, img, image.Point{x, y}, draw.Src)
-
-	// Save the cropped image to a temporary file
-	tmpFile, err := os.CreateTemp("", "cropped_*.jpg")
-	if err != nil {
-		return fmt.Errorf("error creating temporary file: %w", err)
-	}
-
-	err = jpeg.Encode(tmpFile, croppedImg, nil)
-	if err != nil {
-		return fmt.Errorf("error encoding cropped image: %w", err)
-	}
-
-	// Close the temporary file to ensure it is written
-	err = tmpFile.Close()
-	if err != nil {
-		return fmt.Errorf("error closing temporary file: %w", err)
-	}
-
-	// Run Tesseract on the cropped image
-	var result bytes.Buffer
-	cmd := exec.Command("tesseract", tmpFile.Name(), "-", "stdout")
-	cmd.Stdout = &result
-	err = cmd.Run()
-	if err != nil {
-		return fmt.Errorf("error running tesseract: %w", err)
-	}
 	p.text = result.String()
 	return nil
 }
@@ -158,4 +102,8 @@ func (p *Parser) ParseCitizen() (*types.Person, error) {
 func (p *Parser) SetImage(image string) {
 	p.image = image
 	p.GetContent()
+}
+
+func (p *Parser) String() string {
+	return p.text
 }
