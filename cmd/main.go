befe28e6f5d6ff8cbd83d3b924024fe203a2a81a
Admiral-Simo <admiralsimo16@gmail.com>
2024-06-14 12:54:47 +0100

    run the api

--- cmd/main.go
+++ cmd/main.go
@@ -1,40 +1,8 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"log"
-	"os"
-	"passport_card_analyser/internal/adapters/core/ocr"
-	"passport_card_analyser/internal/adapters/core/types"
-	"passport_card_analyser/internal/adapters/core/utilities"
-)
+import "passport_card_analyser/internal/adapters/app/api"
 
 func main() {
-	flag.Parse()
-
-	passports := flag.Args()
-
-	if len(passports) == 0 {
-		log.Fatalf("Usage: %s --passport example_image.jpeg", os.Args[0])
-	}
-
-	people := []*types.Person{}
-
-	parser := ocr.NewParser()
-	for _, card := range passports {
-		parser.SetImage(card)
-		person, err := parser.ParseCitizen()
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		people = append(people, person)
-	}
-
-	for _, person := range people {
-		fmt.Println("----------------------")
-		utilities.PrintPerson(person)
-	}
-	fmt.Println("----------------------")
+    api := api.NewAdapter(":8080")
+	api.Run()
 }
