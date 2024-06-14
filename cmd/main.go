1672e7f52ca62d1869cbab880fc676000cccf4dd
Admiral-Simo <admiralsimo16@gmail.com>
2024-06-14 17:54:58 +0100

    fix: create the uploads directory if it doesn't exist already

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "passport_card_analyser/internal/adapters/app/api"
+import (
+	"log"
+	"os"
+	"passport_card_analyser/internal/adapters/app/api"
+)
 
 func main() {
-    api := api.NewAdapter(":8080")
+	// Define the directory path
+	uploadDir := "uploads"
+
+	// Check if the directory exists
+	if _, err := os.Stat(uploadDir); os.IsNotExist(err) {
+		// Create the directory if it doesn't exist
+		err := os.Mkdir(uploadDir, 0755) // 0755 is the permission for the directory
+		if err != nil {
+			log.Fatalf("Failed to create directory: %v", err)
+		}
+	}
+
+	api := api.NewAdapter(":8080")
 	api.Run()
 }
