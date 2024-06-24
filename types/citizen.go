1eeb9650093ff22ae3e51f94e39e6a03121e247a
Admiral-Simo <admiralsimo16@gmail.com>
2024-06-24 15:36:37 +0100

    update the citizen model

--- types/citizen.go
+++ types/citizen.go
@@ -5,12 +5,18 @@ import (
 )
 
 type Person struct {
-	CNIE                 string    `json:"cin" gorm:"primaryKey;column:cnie"`
-	FirstName            string    `json:"first_name" gorm:"column:first_name"`
-	LastName             string    `json:"last_name" gorm:"column:last_name"`
-	City                 string    `json:"city" gorm:"column:city"`
-	Nationality          string    `json:"nationality" gorm:"column:nationality"`
-	BirthDate            time.Time `json:"birth_date" gorm:"column:birth_date"`
-	ExpireDate           time.Time `json:"expire_date" gorm:"column:expire_date"`
-	PossibleNamesAddress []string  `gorm:"type:text[]" json:"possible_names_address"`
+	CNIE                 string         `json:"cin" gorm:"primaryKey;column:cnie"`
+	FirstName            string         `json:"first_name" gorm:"column:first_name"`
+	LastName             string         `json:"last_name" gorm:"column:last_name"`
+	City                 string         `json:"city" gorm:"column:city"`
+	Nationality          string         `json:"nationality" gorm:"column:nationality"`
+	BirthDate            time.Time      `json:"birth_date" gorm:"column:birth_date"`
+	ExpireDate           time.Time      `json:"expire_date" gorm:"column:expire_date"`
+	PossibleNamesAddress []PossibleName `json:"possible_names_address" gorm:"foreignKey:PersonCNE;references:CNIE"`
+}
+
+type PossibleName struct {
+	ID        uint   `gorm:"primaryKey"`
+	PersonCNE string `gorm:"index;column:person_cnie"`
+	Name      string `json:"name" gorm:"column:name"`
 }
