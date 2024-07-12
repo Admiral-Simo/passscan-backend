aa0d6b76d6481db513c320de3c6781121201f46c
Admiral-Simo <admiralsimo16@gmail.com>
2024-07-13 00:03:22 +0100

    remove unnecessary tests

--- internal/adapters/framework/left/httpadapter/image_extension_utils_test.go
+++ internal/adapters/framework/left/httpadapter/image_extension_utils_test.go
@@ -19,14 +19,3 @@ func TestCheckImage(t *testing.T) {
 	assert.False(t, checkImage("something.txt"), "should be false")
 	assert.False(t, checkImage("something.zip"), "should be false")
 }
-
-func TestExtractExtension(t *testing.T) {
-	assert.Equal(t, ".png", extractExtension("something.png"), "should be equal")
-	assert.Equal(t, ".jpg", extractExtension("something.jpg"), "should be equal")
-	assert.Equal(t, ".jfif", extractExtension("something.jfif"), "should be equal")
-	assert.Equal(t, ".pjpeg", extractExtension("something.pjpeg"), "should be equal")
-	assert.Equal(t, ".pjp", extractExtension("something.pjp"), "should be equal")
-	assert.Equal(t, ".avif", extractExtension("something.avif"), "should be equal")
-	assert.Equal(t, ".gif", extractExtension("something.gif"), "should be equal")
-	assert.Equal(t, ".png", extractExtension("something.png"), "should be equal")
-}
