89ea1490b7f67efc84bf720ee27306a62edc713f
Admiral-Simo <admiralsimo16@gmail.com>
2024-07-13 00:21:54 +0100

    fix bug: Add function to port

--- internal/ports/httpadapter.go
+++ internal/ports/httpadapter.go
@@ -4,5 +4,7 @@ import "net/http"
 
 type HttpPort interface {
 	HandleGetDocumentData(w http.ResponseWriter, r *http.Request)
+	HandleGetUploadHistory(w http.ResponseWriter, r *http.Request)
+	HandleGetImage(w http.ResponseWriter, r *http.Request)
 	Run(portString string)
 }
